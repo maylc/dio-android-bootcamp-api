a40a691585ec8f411dc479b4aa51e548591b239a
Mayara Felix <maylc.dev@gmail.com>
2021-09-15 23:40:35 -0400

    Create adapter and item layout and display list on the screen.

--- Application/app/src/main/java/io/github/maylcf/application/MainActivity.kt
+++ Application/app/src/main/java/io/github/maylcf/application/MainActivity.kt
@@ -1,8 +1,15 @@
 package io.github.maylcf.application
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.github.maylcf.application.api.MyRetrofit
+import io.github.maylcf.application.model.Product
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +20,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         productsRecyclerView = findViewById(R.id.products_recycler)
+        productsRecyclerView.layoutManager = LinearLayoutManager(this)
+        getData()
+    }
+
+    private fun getData() {
+        val call: Call<List<Product>> =
+            MyRetrofit.instance?.productApi()?.getProductApi() as Call<List<Product>>
+
+        call.enqueue(object : Callback<List<Product>> {
+            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+                response.body()?.toList()?.let {
+                    val adapter = ProductAdapter(this@MainActivity, it)
+                    productsRecyclerView.adapter = adapter
+                } ?: run {
+                    Toast.makeText(this@MainActivity, "Error Fetching List", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 }
